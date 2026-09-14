860400af25529a9b2a3258fcf2de7d0f610fdf84
robertomduranf <robertomduranf@gmail.com>
2026-09-14 09:50:52 -0500

    Update sw.js

--- RDFincontrol/sw.js
+++ RDFincontrol/sw.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 var CACHE_PREFIX = 'fincontrol-';
-var CACHE_NAME = CACHE_PREFIX + 'app-v1.5.4';
+var CACHE_NAME = CACHE_PREFIX + 'app-v1.5.5';
 
 var APP_SHELL = [
   './',
